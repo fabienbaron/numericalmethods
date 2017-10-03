6a950105002ab5667aa7cf1dc779e2b4968c6766
Fabien Baron <baron@chara.gsu.edu>
2017-10-03 11:18:34 -0400

    Update

--- tikhonov2.jl
+++ tikhonov2.jl
@@ -3,11 +3,11 @@ include("view.jl")
 x0=read(FITS("saturn64.fits")[1]);
 x0 = vec(x0); # note: x0 is a 2D array, but we will work with vectors
 sigma= maximum(x0)/2*rand(size(x0))
-b=x0+sigma.*randn(size(x0));
+y=x0+sigma.*randn(size(x0));
 W=spdiagm(1./sigma.^2);
-chi2 = sum( (x0-b).^2./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
-chi2 = norm((x0-b)./sigma)^2 # using the l2 norm squared
-chi2 = (x0-b)'*W*(x0-b) # the matricial form for any sigma
+chi2 = sum( (x0-y).^2./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
+chi2 = norm((x0-y)./sigma)^2 # using the l2 norm squared
+chi2 = (x0-y)'*W*(x0-y) # the matricial form for any sigma
 
 # Tikhonov solution
 λ = 2.^(linspace(-100,100,201))
@@ -19,8 +19,8 @@ xopt = zeros(size(x0));
 dist = 1e99
 
 for i=1:nλ
-    x=(W+λ[i]*I)\(W*b)
-    chi2[i] = (x-b)'*W*(x-b)/length(x0)
+    x=(W+λ[i]*I)\(W*y)
+    chi2[i] = (x-y)'*W*(x-y)/length(x0)
     reg[i] = norm(x,2)^2
     clf();
     println("It: ", i, " ", λ[i], " dist: ", norm(x-x0,1));
@@ -36,7 +36,7 @@ clf();
 scatter(reg,chi2)
 xlabel("Regularization")
 ylabel("Chi2")
-imview3(x0,b,xopt,figtitle="Tikhonov regularization");
+imview3(x0,y,xopt,figtitle="Tikhonov regularization");
 
 
 nx = 64;
@@ -46,8 +46,8 @@ D = [kron(speye(nx), D_1D) ;  kron(D_1D, speye(nx))];
 DtD = D'*D;
 
 for i=1:nλ
-    x=(W+λ[i]*DtD)\(W*b)
-    chi2[i] = (x-b)'*W*(x-b)/length(x0)
+    x=(W+λ[i]*DtD)\(W*y)
+    chi2[i] = (x-y)'*W*(x-y)/length(x0)
     reg[i] = norm(x,2)^2
     clf();
     println("It: ", i, " ", λ[i], " dist: ", norm(x-x0,1));
@@ -62,4 +62,4 @@ clf();
 scatter(reg,chi2)
 xlabel("Regularization")
 ylabel("Chi2")
-imview3(x0,b,xopt,figtitle="Tikhonov regularization");
+imview3(x0,y,xopt,figtitle="Tikhonov regularization");
