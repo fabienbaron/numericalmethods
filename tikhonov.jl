554360db89322d95dfc3fcb87e30a410290b2283
Fabien Baron <baron@chara.gsu.edu>
2017-10-03 10:06:35 -0400

    Prep for class

--- tikhonov.jl
+++ tikhonov.jl
@@ -1,18 +1,15 @@
 using FITSIO
 include("view.jl")
-x0=read(FITS("saturn.fits")[1]);
+x0=read(FITS("saturn64.fits")[1]);
 x0 = vec(x0); # note: x0 is a 2D array, but we will work with vectors
-sigma= maximum(x0)/5*rand(size(x0))
+sigma= maximum(x0)/2*rand(size(x0))
 b=x0+sigma.*randn(size(x0));
 W=spdiagm(1./sigma.^2);
 chi2 = sum( (x0-b).^2./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
+chi2 = norm((x0-b)./sigma)^2 # using the l2 norm squared
 chi2 = (x0-b)'*W*(x0-b) # the matricial form for any sigma
-
 # Tikhonov solution
 λ = 1e-4
-x=(W+λ*I)\(W*b);
-
-imview3(x0,b,x,figtitle="Tikhonov regularization");
-
-norm(x-x0,1);
-norm(x-x0,2);
+x=(W+λ[i]*I)\(W*b)
+xopt = x.*(x.>0)
+imview3(x0,b,xopt,figtitle="Tikhonov regularization");
