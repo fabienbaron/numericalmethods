5655e5f2e330cb3ebe9266b73ecd24cd62f9d778
Fabien Baron <baron@chara.gsu.edu>
2017-10-19 10:51:52 -0400

    Added boostrap ellipse example

--- bootstrap_ellipse.jl
+++ bootstrap_ellipse.jl
@@ -1,6 +1,8 @@
+using PyPlot
 # Generate data
-θ=rand(20)*pi;
+θ=rand(20)*2*pi;
 x=3*cos.(θ)+.5*randn(20); y = 5*sin.(θ)+.5*randn(20);
+scatter(x,y);
 
 #Visualize the true ellipse
 θ_all=linspace(-pi,pi,1000)
