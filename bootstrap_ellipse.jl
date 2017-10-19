4b8314ac80d7fa3127c0c5e3011a87d31c50551a
Fabien Baron <baron@chara.gsu.edu>
2017-10-19 11:40:04 -0400

    Update to boostrap

--- bootstrap_ellipse.jl
+++ bootstrap_ellipse.jl
@@ -15,21 +15,57 @@ scatter(3*cos.(θ_all), 5*sin.(θ_all))
 
 # Grid sample for a and b
 
-a = linspace(-10, 10, 100);
-b = linspace(-10, 10, 100);
-chi2 = zeros(100,100);
+a = linspace(0, 8, 400);
+b = linspace(0, 8, 400);
+chi2 = zeros(length(a),length(b));
 
 for i=1:length(a)
     for j=1:length(b)
-        chi2[i,j] = sum( (x-a[i]*cos.(θ)).^2  + (y-b[j]*cos.(θ)).^2)/σ^2
+        chi2[i,j] = sum( (x-a[i]*cos.(θ)).^2  + (y-b[j]*sin.(θ)).^2)/σ^2
     end
 end
 
 minpos = ind2sub(size(chi2), indmin(chi2))
 println("Minimun at: a=", a[minpos[1]], " b = ", b[minpos[2]])
-imshow(chi2)
+clf(); imshow(rotl90(chi2),interpolation="none");
+xlabel("a");ylabel("b");
 
 # Compute error bars from chi2
+# TODO !
+
+
+
+# Estimate best (a,b) with NLopt
+using NLopt
+function chi2opt(params::Vector, dummy::Vector)
+    return sum( (x-params[1]*cos.(θ)).^2  + (y-params[2]*sin.(θ)).^2)/σ^2
+end
+
+params_init = [0,0];
+opt = Opt(:LN_NELDERMEAD, 2);
+min_objective!(opt, chi2opt);
+(minchi2,params_opt,ret) = optimize(opt, params_init);
+println("got $minchi2 at $params_opt (returned $ret)")
+
+# Estimate best (a,b) with NLopt
+using NLopt
+function chi2opt(params::Vector, dummy::Vector)
+    return sum( (x-params[1]*cos.(θ)).^2  + (y-params[2]*sin.(θ)).^2)/σ^2
+end
+
+params_init = [0,0]
+opt = Opt(:LN_NELDERMEAD, 2)
+min_objective!(opt, chi2opt)
+(minchi2,params_opt,ret) = optimize(opt, params_init)
+println("got $minchi2 at $params_opt (returned $ret)");
+
+
+
+
+
+
+
+
 
 
 
