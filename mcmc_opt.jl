bdaf69dfb54d4ccc7965cb0a4df97276b5873351
Fabien Baron <baron@chara.gsu.edu>
2017-11-28 10:59:50 -0500

    Updated optim

--- mcmc_opt.jl
+++ mcmc_opt.jl
@@ -17,20 +17,19 @@ y_y = repmat(x_x',1,1);
 ackley_map = ackley(x_x,y_y);#rosenbrock(x_x,y_y,1,100);
 
 # Metropolis-Hastings with simulated annealing
-tn = 10.; t0 = 1e-5; N = 1000;
+tn = 10.; t0 = 1e-5; N = 1000000;
 # Random walk
 x = zeros(N);
 y = zeros(N);
-
 x[1] = -4; y[1] = -2;
-t = (tn - t0)/2 * (1 + cos(collect(1:N)*pi/N)); #t *= 0.1;
-#ackley_vals = [];
-
+N1=10000
+t=zeros(N)
+t[1:N1] = (tn - t0)/2 * (1 + cos(collect(1:N1)*pi/N1)); #t *= 0.1;
 clf();
 imshow(ackley_map)
-
+tight_layout();
 for i = 2:N
-    stepscale=0.4;
+    stepscale=0.4#0.001+5*exp(-i/1000);
     x_trial = max(min(x[i-1] + stepscale*(rand()*2 - 1),5),-5);
     y_trial = max(min(y[i-1] + stepscale*(rand()*2 - 1),5),-5);
     e_trial = ackley(x_trial,y_trial);
@@ -38,7 +37,7 @@ for i = 2:N
 
     # choose number 6 - temperature schedule
     a = min(1, exp(-(e_trial - e_current)/t[i]));
-    println("iteration: $i  acceptance: $a")
+    println("iteration: $i  acceptance: $a stepscale: $stepscale")
     if (rand() < a) # Accept
         x[i] = deepcopy(x_trial);
         y[i] = deepcopy(y_trial);
@@ -46,8 +45,7 @@ for i = 2:N
         x[i] = deepcopy(x[i-1]);
         y[i] = deepcopy(y[i-1]);
     end
-
-    plot((x[i-1:i]+5)*100,(y[i-1:i]+5)*100);
+#    plot((x[i-1:i]+5)*99+1,(y[i-1:i]+5)*99+1);
     #readline();
 end
 
