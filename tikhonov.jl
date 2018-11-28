7a76ff8f0006a37a8a11c334707f4bb00b95c436
Fabien Baron <baron@chara.gsu.edu>
2018-11-28 12:07:32 -0500

    Updated Tikhnov examples for Julia 1.0

--- tikhonov.jl
+++ tikhonov.jl
@@ -1,21 +1,25 @@
 using FITSIO
+using LinearAlgebra
 include("view.jl")
 x0=read(FITS("saturn64.fits")[1]);
 x0 = vec(x0); # note: x0 is a 2D array, but we will work with vectors
-sigma= maximum(x0)/2*rand(size(x0))
-y=x0+sigma.*randn(size(x0));
-W=spdiagm(1./sigma.^2);
-#chi2 = sum( (x0-y).^2./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
-#chi2 = norm((x0-y)./sigma)^2 # using the l2 norm squared
-#chi2 = (x0-y)'*W*(x0-y) # the matricial form for any sigma
-# Tikhonov solution
-λ = 10.^(linspace(-10,10,21));
-nλ = length(λ);
-mindist = 1e99
-xopt = zeros(size(x0));
+sigma= maximum(x0)/2*rand(Float64, size(x0))
+y=x0+sigma.*randn(Float64,size(x0));
+W=Diagonal(1.0./sigma.^2);
 
-chi2 = zeros(nλ)
-reg =  zeros(nλ)
+
+
+chi2 = sum( (x0-y).^2 ./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
+chi2 = norm((x0-y)./sigma)^2 # using the l2 norm squared
+chi2 = (x0-y)'*W*(x0-y) # the matricial form for any sigma
+
+# Tikhonov solution
+λ = 10.0.^(range(-10,10,length=21));
+nλ = length(λ);
+global mindist = 1e99
+#global xopt = zeros(size(x0));
+global chi2 = zeros(nλ)
+global reg =  zeros(nλ)
 
 for i=1:nλ
     x=(W+λ[i]*I)\(W*y)
@@ -25,12 +29,9 @@ for i=1:nλ
     dist = norm(xpos-x0,1);
     println("i= $i ", dist);
     if dist<mindist
-        mindist = copy(dist);
-        xopt = copy(xpos);
+        global mindist = deepcopy(dist);
+        global xopt = deepcopy(xpos);
     end
-
-
-
 end
 
 imview3(x0,y,xopt,figtitle="Tikhonov regularization");
