97c40c7b5907b455949b9a394a4a8aefaf7c52db
Fabien Baron <baron@chara.gsu.edu>
2019-05-02 10:01:43 -0400

    Updated notation

--- tikhonov2.jl
+++ tikhonov2.jl
@@ -6,11 +6,11 @@ x0=read(FITS("saturn64.fits")[1]);
 x0 = vec(x0); # note: x0 is a 2D array, but we will work with vectors
 sigma= maximum(x0)/2*rand(Float64, size(x0))
 y=x0+sigma.*randn(Float64,size(x0));
-W=Diagonal(1.0./sigma.^2);
+C=Diagonal(1.0./sigma.^2); # covariance matrix
 
 chi2 = sum( (x0-y).^2 ./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
 chi2 = norm((x0-y)./sigma)^2 # using the l2 norm squared
-chi2 = (x0-y)'*W*(x0-y) # the matricial form for any sigma
+chi2 = (x0-y)'*C*(x0-y) # the matricial form for any sigma
 
 
 # Tikhonov solution
@@ -21,8 +21,8 @@ global chi2 = zeros(nλ);
 global reg =  zeros(nλ);
 
 for i=1:nλ
-    x=(W+λ[i]*I)\(W*y)
-    chi2[i] = ((x-y)'*W*(x-y))[1]/length(x0)
+    x=(C+λ[i]*I)\(C*y)
+    chi2[i] = ((x-y)'*C*(x-y))[1]/length(x0)
     reg[i] = norm(x,2)^2;
     xpos = x.*(x.>0)
     dist = norm(xpos-x0,1);
@@ -48,8 +48,8 @@ DtD = D'*D;
 global mindist = 1e99;
 
 for i=1:nλ
-    x=(W+λ[i]*DtD)\(W*y)
-    chi2[i] = ((x-y)'*W*(x-y))[1]/length(x0)
+    x=(C+λ[i]*DtD)\(C*y)
+    chi2[i] = ((x-y)'*C*(x-y))[1]/length(x0)
     reg[i] = norm(x,2)^2
     xpos = x.*(x.>0)
     dist = norm(xpos-x0,1);
@@ -58,11 +58,11 @@ for i=1:nλ
         global xopt = deepcopy(xpos);
     end
     @printf("It: %3i λ:%8.1e chi2: %8.2f dist: %8.2f\n", i,λ[i],chi2[i], dist  );
-    imview(reshape(xpos,64,64))
-    readline();
+    #imview(reshape(xpos,64,64))
+    #readline();
 end
 clf();
 scatter(reg,chi2)
 xlabel("Regularization")
 ylabel("Chi2")
-imview3(x0,y,xopt,figtitle="Tikhonov regularization");
+imview3(x0,y,xopt,figtitle="Total squared variation regularization");
