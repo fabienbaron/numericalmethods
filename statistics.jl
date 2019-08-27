639c87ec669f9bd945964ea1816329f99b0d4ca2
FB <baron@chara.gsu.edu>
2019-08-27 15:44:11 -0400

    Start of semester

--- statistics.jl
+++ statistics.jl
@@ -1,14 +1,10 @@
 using PyPlot
 x = randn(100000) # Values
 nbins = 100; # Number of bins
-h = plt[:hist](x,nbins) # Histogram
+h = hist(x,bins=nbins); # Histogram
 grid("on");xlabel("X");ylabel("Occurences");title("Probability histogram")
 
-x = rand(100000) # Values
+x = rand(100000); # Values
 nbins = 100; # Number of bins
-h = plt[:hist](x,nbins) # Histogram
+h = hist(x,bins=nbins); # Histogram
 grid("on");xlabel("X");ylabel("Occurences");title("Probability histogram")
-
-lambda = 1.
-y = rand(100000)
-x = -log.(1-y)/lambda
