05ebe44b6c896e948d17ee8db3a39c64a69cc52e
Fabien Baron <baron@phy-astr.gsu.edu>
2019-09-06 15:02:54 -0400

    Updated PDF

--- statistics.jl
+++ statistics.jl
@@ -1,5 +1,7 @@
 using PyPlot
-x = randn(100000) # Values
+μ = 5.0
+σ = 3.0
+x = μ .+ σ * randn(100000) # Values
 nbins = 100; # Number of bins
 h = hist(x,bins=nbins); # Histogram
 grid("on");xlabel("X");ylabel("Occurences");title("Probability histogram")
