fe64d537d81037e1f5b6688ef8ae7ad1ae90a08c
Fabien Baron <baron@phy-astr.gsu.edu>
2019-09-18 15:39:27 -0400

    End of statistics chapter

--- tikhonov2.jl
+++ tikhonov2.jl
@@ -8,37 +8,50 @@ sigma= maximum(x0)/2*rand(Float64, size(x0))
 y=x0+sigma.*randn(Float64,size(x0));
 C=Diagonal(1.0./sigma.^2); # covariance matrix
 
+# Chi2 and reduced chi2
+#
 chi2 = sum( (x0-y).^2 ./sigma.^2) # the conventional way to write the chi2 for diagonal sigma
 chi2 = norm((x0-y)./sigma)^2 # using the l2 norm squared
 chi2 = (x0-y)'*C*(x0-y) # the matricial form for any sigma
+chi2r = chi2/length(y)
 
-
-# Tikhonov solution
-λ = 10.0.^(range(-20,20,length=41));
+# Classic Tikhonov solution
+λ = 10.0.^(range(-15,15,length=201));
 nλ = length(λ);
 global mindist = 1e99;
 global chi2 = zeros(nλ);
 global reg =  zeros(nλ);
+global obj =  zeros(nλ);
 
 for i=1:nλ
-    x=(C+λ[i]*I)\(C*y)
-    chi2[i] = ((x-y)'*C*(x-y))[1]/length(x0)
+    x=(C+λ[i]*I)\(C*y);
+    #x = x.*(x.>0)
+    chi2[i] = ((x-y)'*C*(x-y))[1]
     reg[i] = norm(x,2)^2;
-    xpos = x.*(x.>0)
-    dist = norm(xpos-x0,1);
+    obj[i] = chi2[i] + λ[i]*reg[i];
+    dist = norm(x-x0,1);
     if dist<mindist
         global mindist = deepcopy(dist);
-        global xopt = deepcopy(xpos);
+        global xopt = deepcopy(x);
     end
-    @printf("It: %3i λ:%8.1e chi2: %8.2f dist: %8.2f\n", i,λ[i],chi2[i], dist  );
+    @printf("It: %3i obj:%8.1e λ:%8.1e chi2r: %5.2f chi2: %8.2f  λ*reg: %8.2f reg: %8.2f dist: %8.2f\n", i, λ[i], obj[i], chi2[i]/length(y), chi2[i], reg[i], λ[i]*reg[i], dist  );
 end
 
-clf();
-scatter(reg,chi2)
+# Plot L curve
+fig = figure("L Curve",figsize=(15,5))
+clf()
+scatter(λ.*reg,chi2)
+gca().set_yscale("log")
+gca().set_xscale("log")
 xlabel("Regularization")
 ylabel("Chi2")
+
+# Display object, noisy image and reconstruction
 imview3(x0,y,xopt,figtitle="Tikhonov regularization");
 
+#
+# Total squared variation
+#
 using SparseArrays
 nx = 64;
 o = ones(nx);
