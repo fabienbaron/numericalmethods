19161f9b668df659d3d515b3a4e4768b741cf973
Fabien Baron <baron@phy-astr.gsu.edu>
2019-10-16 12:02:12 -0400

    Updated bootstrap example

--- src/bootstrap_ellipse.jl
+++ src/bootstrap_ellipse.jl
@@ -8,36 +8,41 @@ x=a*cos.(θ)+σ*randn(20); y = b*sin.(θ)+σ*randn(20);
 scatter(x,y);
 
 #Visualize the true ellipse
-θ_all=linspace(-pi,pi,1000)
+θ_all=range(-pi,pi,length=1000)
 scatter(3*cos.(θ_all), 5*sin.(θ_all))
 
 # Given data (x,y), estimate a and b
 
 # Grid sample for a and b
 
-# a = linspace(0, 8, 400);
-# b = linspace(0, 8, 400);
-# chi2 = zeros(length(a),length(b));
-#
-# for i=1:length(a)
-#     for j=1:length(b)
-#         chi2[i,j] = sum( (x-a[i]*cos.(θ)).^2  + (y-b[j]*sin.(θ)).^2)/σ^2
-#     end
-# end
-#
-# minpos = ind2sub(size(chi2), indmin(chi2))
-# println("Minimun at: a=", a[minpos[1]], " b = ", b[minpos[2]])
-# clf(); imshow(rotl90(chi2),interpolation="none");
-# xlabel("a");ylabel("b");
+ a = collect(range(0, 8, length=400));
+ b = collect(range(0, 8, length=400));
+ chi2 = zeros(length(a),length(b));
 
-# Compute error bars from chi2
-# TODO !
+ for i=1:length(a)
+     for j=1:length(b)
+         chi2[i,j] = sum( (x-a[i]*cos.(θ)).^2  + (y-b[j]*sin.(θ)).^2)/σ^2
+     end
+ end
+ minsol = findmin(chi2)
+ minchi2 = minsol[1]
+ println("Minimun chi2 = ", minchi2," at: a=", a[minsol[2][1]], " b = ", b[minsol[2][2]])
+ clf(); imshow(rotl90(chi2),interpolation="none");
+ xlabel("a");ylabel("b");
+ imshow(rotl90(chi2.>(minchi2+1.0)),interpolation="none");
+ rangea = findall(vec(sum(chi2.<(minsol[1]+1.0), dims=2)).>0)
+ rangeb = findall(vec(sum(chi2.<(minsol[1]+1.0), dims=1)).>0)
+ a[rangea]
+ b[rangeb]
+ print("a = ", a[minsol[2][1]], " + ", maximum(a[rangea])-a[minsol[2][1]] , "- ",  a[minsol[2][1]]-minimum(a[rangea]))
+ print("b = ", b[minsol[2][2]], " + ", maximum(b[rangeb])-b[minsol[2][2]] , "- ",  b[minsol[2][2]]-minimum(b[rangeb]))
+
 
 # Estimate best (a,b) with NLopt
 using NLopt
 
 function chi2opt(params::Vector, dummy::Vector)
-    return sum( (x-params[1]*cos.(θ)).^2  + (y-params[2]*sin.(θ).^2)/σ^2)
+    return sum( (x-params[1]*cos.(θ)).^2  + (y-params[2]*sin.(θ)).^2)/σ^2
 end
 
 params_init = [0,0];
@@ -89,9 +94,20 @@ end
 
 # Compute error bars from bootstrap
 clf();
-ha = plt[:hist](a_boot,50) # Histogram
-hb = plt[:hist](b_boot,50) # Histogram
+ha = hist(a_boot,100) # Histogram
+hb = hist(b_boot,100) # Histogram
 
+ha[1]; #histogram data: counts per intervals
+ha[2]; #histogram data: intervals
+mode, indx_mode = findmax(ha[1]); # mode of the distribution
+indx = findall(ha[1][:].>0.5*mode) # index range where > half max
+indx_HM_left = minimum(indx);
+indx_HM_right = maximum(indx);
+center = (ha[2][indx_mode]+ha[2][indx_mode])/2
+FWHM_low = center - (ha[2][indx_HM_left]+ha[2][indx_HM_left+1])/2
+FWHM_high = (ha[2][indx_HM_right]+ha[2][indx_HM_right+1])/2 - center
+σ_low =  FWHM_low / (2*sqrt(2*log(2))) # Assuming left ~ Gaussian
+σ_high = FWHM_high / (2*sqrt(2*log(2))) # Assuming right ~ Gaussian
 #
 # CLASSIC JACKNIFE
 #
