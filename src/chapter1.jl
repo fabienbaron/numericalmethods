05d21402d51809b681dc1406045072068d138dd4
Fabien Baron <fbaron@gsu.edu>
2021-08-31 09:09:07 -0400

    Added chapter11.jl

---
+++ src/chapter1.jl
@@ -0,0 +1,31 @@
+# This code is meant to be input line by line into the REPL
+using Distributions, PyPlot
+d = Uniform(0,1) # the most basic Distribution
+x = rand(d, 100000)
+hist(x,bins=100)
+# Statistics on the sample
+mean(x)
+median(x)
+var(x)
+# Statistics on the population
+mean(d)
+median(d)
+var(d)
+
+#PDF and CDF
+pdf(d,0.3)
+cdf(d,0.3)
+ccdf(d,0.3)
+logpdf(d,0.3)
+
+# Dice!
+d = DiscreteUniform(1,6)
+x = rand(d, 100000)
+hist(x,bins=100)
+pdf(d,2)
+std(d)
+
+# Normal distribution, e.g. IQ distribution
+d = Normal(100,15)
+std(d)
+x = rand(d, 100000)
