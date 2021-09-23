2cdbe9c3da39bd763d716eab9bf5745805aba52c
Fabien Baron <fbaron@gsu.edu>
2021-09-23 09:23:22 -0400

    Update to linear deconv

--- src/tikhonov_denoising.jl
+++ src/tikhonov_denoising.jl
@@ -16,10 +16,10 @@ chi2r = chi2/length(y)
 # Classic Tikhonov "ridge regression"
 λ = 10.0.^(range(-10,15,length=201));
 nλ = length(λ);
-global mindist = 1e99;
-global chi2 = zeros(nλ);
-global reg =  zeros(nλ);
-global obj =  zeros(nλ);
+mindist = 1e99;
+chi2 = zeros(nλ);
+reg =  zeros(nλ);
+obj =  zeros(nλ);
 
 A = I;
 Γ = I;
@@ -32,8 +32,8 @@ for i=1:nλ
     obj[i] = chi2[i] + λ[i]*reg[i];
     dist = norm(x-x0,1);
     if dist<mindist
-        global mindist = deepcopy(dist);
-        global xopt = deepcopy(x);
+         mindist = deepcopy(dist);
+         xopt = deepcopy(x);
     end
     @printf("It: %3i obj:%8.1e λ:%8.1e chi2r: %5.2f chi2: %8.2f  λ*reg: %8.2f reg: %8.2f dist: %8.2f\n",  i, obj[i], λ[i], chi2[i]/length(y), chi2[i], reg[i], λ[i]*reg[i], dist  );
     #imview(reshape(x, (nx,nx)))
@@ -85,7 +85,7 @@ o = ones(nx); D_1D = spdiagm(-1=>-o[1:nx-1],0=>o)
 A = I;
 Γ = ∇;
 
-global mindist = 1e99;
+ mindist = 1e99;
 for i=1:nλ
     x=(A'*Σ*A+λ[i]*Γ'*Γ)\(A'*Σ*y);
     x = x.*(x.>0)
@@ -94,8 +94,8 @@ for i=1:nλ
     obj[i] = chi2[i] + λ[i]*reg[i];
     dist = norm(x-x0,1);
     if dist<mindist
-        global mindist = deepcopy(dist);
-        global xopt = deepcopy(x);
+         mindist = deepcopy(dist);
+         xopt = deepcopy(x);
     end
     @printf("It: %3i obj:%8.1e λ:%8.1e chi2r: %5.2f chi2: %8.2f  λ*reg: %8.2f reg: %8.2f dist: %8.2f\n",  i, obj[i], λ[i], chi2[i]/length(y), chi2[i], reg[i], λ[i]*reg[i], dist  );
     #imview(reshape(x, (nx,nx)))
