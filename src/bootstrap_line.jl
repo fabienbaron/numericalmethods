e9df44f2be5d22f36b9bd5874856576cbd745a3f
Fabien Baron <fbaron@gsu.edu>
2021-10-07 09:53:40 -0400

    Added line bootstrap example

--- src/bootstrap_line.jl
+++ src/bootstrap_line.jl
@@ -1,8 +1,12 @@
+using PyPlot;
+using LinearAlgebra
+
 N=30
 σ=rand(N);
 X=sort(rand(N));
 θ1=5
 θ2=7
 Y=θ1 .+ θ2*X + σ.*randn(N);
-using PyPlot;
-scatter(X,Y,s=3)
+scatter(X,Y)
+
+chi2=θ->norm((Y - (θ[1] .+ θ[2]*X) )./σ)^2
