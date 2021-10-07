312b73ee9312535ab59c480a5e437b041c5a5ee1
Fabien Baron <fbaron@gsu.edu>
2021-10-07 10:05:57 -0400

    Added line bootstrap example

--- src/bootstrap_line.jl
+++ src/bootstrap_line.jl
@@ -1,6 +1,4 @@
-using PyPlot;
-using LinearAlgebra
-
+using PyPlot, LinearAlgebra, NLopt
 N=30
 σ=rand(N);
 X=sort(rand(N));
@@ -8,5 +6,11 @@ X=sort(rand(N));
 θ2=7
 Y=θ1 .+ θ2*X + σ.*randn(N);
 scatter(X,Y)
+chi2=(θ,dummy)->norm((Y - (θ[1] .+ θ[2]*X) )./σ)^2
 
-chi2=θ->norm((Y - (θ[1] .+ θ[2]*X) )./σ)^2
+#Minimization
+θ_init = [1.0,1.0];
+optimizer = Opt(:LN_NELDERMEAD, length(θ_init));
+min_objective!(optimizer, chi2);
+(minchi2,θ_opt,ret) = optimize(optimizer, θ_init);
+println("got $minchi2 at $θ_opt (returned $ret)");
