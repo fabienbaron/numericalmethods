3327372760655f9e835ad5d27b0feebb2ecbb547
Fabien Baron <fbaron@gsu.edu>
2023-09-11 23:37:54 -0400

    Updated chi2 example

--- src/chi2.jl
+++ src/chi2.jl
@@ -11,13 +11,17 @@ y_noiseless = M(θtrue) # Noiseless data
 y = y_noiseless + σ.*randn(N)
 
 scatter(x,y_noiseless, label = "Noiseless truth")
+scatter(x,y)
 errorbar(x,y, σ ,linestyle="none", label="Noisy data");
+legend()
 
 θ = rand(2) # some random value we want to try
 m = M(θ) # model values
 R = (m - y)./σ # residuals
-χ2= θ-> sum((y - M(θ)).^2)  # also = norm(R)^2
-χ2r= θ-> sum((y - M(θ)).^2)/N  # also = norm(R)^2
+χ2= θ-> sum(((y - M(θ))./σ).^2)  # also = norm(R)^2
+χ2r= θ-> sum(((y - M(θ))./σ).^2)/N  # also = norm(R)^2
 
-θ = rand(2) # some random value we want to try
+θ = 20*rand(2) # some random value we want to try
 χ2(θ)
+ytry = M(θ)
+plot(x,ytry)
