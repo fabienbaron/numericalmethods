31fde980fa8e14a5002ed6706ee9b6e60ab7ef14
fabienbaron <baron@phy-astr.gsu.edu>
2023-11-16 12:39:47 -0500

    GP

--- src/gaussian_proc.jl
+++ src/gaussian_proc.jl
@@ -4,20 +4,33 @@ function K(x,xp, σ, l)
 K = zeros(Float64, length(x), length(xp));
 for i=1:length(x)
     for j=1:length(xp)
-        K[i,j]=σ*exp(-0.5*(x[i]-xp[j])^2/l^2)
+        K[i,j]=σ^2*exp(-0.5*(x[i]-xp[j])^2/l^2)
     end
 end
 return K
 end
 
-x=collect(1:100)
-σn=2.0; σ=1.0; l=2.0
-Σ=K(x,x, σ, l)
+f=x->sin(0.1*x)*(x/5)^2
+close(); clf()
+plot(1:100, f.(1:100), linestyle="dotted", label="Truth")
+x = sort(unique(ceil.(Int, rand(30)*100)))
+N = length(x)
+σn=20.0;
+y = f.(x) + σn*randn(N)
+scatter(x,y)
+errorbar(x,y,σn,linestyle="none", label="Data")
+legend()
+
+σn=2.0; σ=100.0; l=30.0
+xp=1:100
+Σ=K(xp,xp, σ, l)
 L=cholesky(Σ).U' # norm(L*L'-Σ) ~ 0
-plot(x,L*rand(100)) # repeat this to plot more samples
-y=L*rand(100);
-xstar=[1.5, 2.5, 3.5, 101, 102];
-plot(x,y)
-fstar=K(xstar, x, σ, l)*((K(x,x,σ,l)+σn*I)\y);
-covfstar=K(xstar, xstar, σ,l)-K(xstar, x, σ, l)*(inv(K(x, x, σ, l)+σn^2*I)*K(x, xstar, σ, l))
+plot(xp,L*rand(100))
+
+xstar=[2.5, 23.5, 50.5, 82.5, 102];
+fstar=K(xstar, x, σ, l)*((K(x,x,σ,l)+σn^2*I)\y);
+covfstar=K(xstar, xstar, σ,l)-K(xstar, x, σ, l)*((K(x, x, σ, l)+σn^2*I)\K(x, xstar, σ, l))
+σstar=sqrt.([covfstar[i,i] for i=1:length(xstar)])
 scatter(xstar, fstar)
+errorbar(xstar, fstar, σstar,linestyle="none", label="Inferred values")
+
