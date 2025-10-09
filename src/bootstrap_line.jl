ae8f257bc7292ef3080834cab8252ea6f2c9b9f0
Fabien Baron <fbaron@gsu.edu>
2025-10-09 11:41:01 -0400

    Updated chap3 + crb.jl

--- src/bootstrap_line.jl
+++ src/bootstrap_line.jl
@@ -2,31 +2,39 @@ using PyPlot, LinearAlgebra, NLopt, Statistics
 N=5
 σ=rand(N);
 X=sort(rand(N));
-θ1=5
-θ2=7
-Y=θ1 .+ θ2*X + σ.*randn(N);
-clf(); 
-scatter(X,Y)
-errorbar(X,Y,σ, linestyle="none")
-chi2=(θ,dummy)->norm((Y - (θ[1] .+ θ[2]*X) )./σ)^2
+θ=[5.0,7.0]
+Y= θ[2] .+ θ[1]*X + σ.*randn(N);
+chi2=(θ,dummy)->norm((Y - (θ[2] .+ θ[1]*X) )./σ)^2
 #Minimization
 θ_init = [1.0,1.0];
 optimizer = Opt(:LN_NELDERMEAD, length(θ_init));
 min_objective!(optimizer, chi2);
-(minchi2,θ_opt,ret) = optimize(optimizer, θ_init);
+(minchi2,θ_hat,ret) = optimize(optimizer, θ_init);
 println("got $minchi2 at $θ_opt (returned $ret)");
+
+# --- Plot data and fitted line ---
+figure(figsize=(7,5))
+scatter(X, Y, label="Data", color="blue")
+errorbar(X,Y,σ, linestyle="none")
+plot(X,     θ[1] * X .+ θ[2], "g--", label="True line")
+plot(X, θ_hat[1] * X .+ θ_hat[2], "r-", label="Estimated line")
+xlabel("x")
+ylabel("y")
+legend()
+title("Linear model with Gaussian noise")
 
 # LsqFit (Levenberg-Marquardt algorithm)
 using LsqFit
-m = (X,θ) ->θ[1] .+ θ[2]*X
-fit = curve_fit(m, X, Y, 1.0./σ.^2 , θ_init)
+m = (X,θ) -> θ[1]*X .+ θ[2] 
+fit = curve_fit(m, X, Y, 1.0./σ.^2 , θ_init);
+θ_hat2 = fit.param
+norm(θ_hat2-θ_hat)
 cov = estimate_covar(fit)
-se = standard_errors(fit)
-margin_of_error = margin_error(fit, 0.3)
-confidence_intervals = confidence_interval(fit, 0.1) # 10%
-
-# Cramer-Rao bound (TBD)
-# This will be completed when we see the CRB and Fisher information
+println(θ_hat2[1], " ± " , sqrt(cov[1,1]), "\t vs truth = ", θ[1])
+println(θ_hat2[2], " ± " , sqrt(cov[2,2]), "\t vs truth = ", θ[2])
+##se = standard_errors(fit)
+#margin_of_error = margin_error(fit, 0.3)
+#confidence_intervals = confidence_interval(fit, 0.1) # 10%
 
 #
 # Jacknife
@@ -50,11 +58,18 @@ for i=1:N
 end
 mean(θ_opt, dims=2) # Mean values -> nominal values
 std(θ_opt, dims=2) # Std -> Jacknife uncertainties
+# Note: sometimes the Jacknife will fail because of outlier
+# in this case median(θ_opt, dims=2) will differ strongly from mean(θ_opt, dims=2)
+# Or use a library. 
+using RobustModels; 
+mean(MEstimator{HuberLoss}(), θ_opt; dims=2); 
+mean_and_std(SEstimator{TukeyLoss}(),θ_opt , dims=2)
+mean_and_std(TauEstimator{YohaiZamarLoss}(),θ_opt , dims=2)
+#
+# Bootstrap with replacement 
+#
 
-#
-# Bootstrap with replacement ->>> WORKS
-#
-Nboot = 1000
+Nboot = 10000
 θ_opt = zeros(Float64, 2, Nboot)
 for i=1:Nboot
     # Change the dataset on which we do minimization
@@ -64,12 +79,17 @@ for i=1:Nboot
     σ = σ_orig[indx]
 
     # Do minimization
-    θ_init = [1.0,1.0];
-    optimizer = Opt(:LN_NELDERMEAD, length(θ_init));
-    min_objective!(optimizer, chi2);
-    (minchi2,θ_opt[:,i],ret) = optimize(optimizer, θ_init);
+    θ_init = [0.0,0.0];
+  #  optimizer = Opt(:LN_NELDERMEAD, length(θ_init));
+  #  min_objective!(optimizer, chi2);
+  #  (minchi2,θ_opt[:,i],ret) = optimize(optimizer, θ_init);
+
+    fit = curve_fit(m, X, Y, 1.0./σ.^2 , θ_init);
+    θ_opt[:,i] = fit.param
+
 end
 mean(θ_opt[1,:])
 std(θ_opt[1,:])
 mean(θ_opt[2,:])
 std(θ_opt[2,:])
+hist(θ_opt[1,:], bins=100);
---
+++ src/crb.jl
@@ -0,0 +1,45 @@
+using LinearAlgebra,  Random, Distributions,  PyPlot
+
+N = 50
+x = range(-2, 2, length=N)
+θ = [1.5, -0.7]
+
+σ = 0.5
+Σ = (σ^2) * I(N) # Covariance matrix (white Gaussian noise)
+
+# Simulate data
+y =  θ[1] * x .+  θ[2] + σ .* randn(N)
+
+# --- Compute Fisher information matrix ---
+X = [x ones(N)]
+Σinv = inv(Σ)
+Iθ = X' * Σinv * X
+
+# Cramér–Rao bound
+CRLB = inv(Iθ)
+
+# --- Estimate parameters by least squares ---
+θ_hat = Iθ \ (X' * Σinv * y)
+
+println("Least squares results with Cramér–Rao bounds: ")
+print(θ_hat[1], " ± ", sqrt(CRLB[1,1]))
+print(θ_hat[2], " ± ", sqrt(CRLB[2,2]))
+
+# Example of a package doing this for you
+using LsqFit
+m = (X,θ) ->θ[2] .+ θ[1]*X
+θ_init = [1.0,1.0] 
+fit = curve_fit(m, x, y, Σinv , θ_init);
+fit.param # results
+cov = estimate_covar(fit) # this gives the same result as CRB
+
+
+# --- Plot data and fitted line ---
+figure(figsize=(7,5))
+scatter(x, y, label="Data", color="blue")
+plot(x,     θ[1] * x .+ θ[2], "g--", label="True line")
+plot(x, θ_hat[1] * x .+ θ_hat[2], "r-", label="Estimated line")
+xlabel("x")
+ylabel("y")
+legend()
+title("Linear model with Gaussian noise")
