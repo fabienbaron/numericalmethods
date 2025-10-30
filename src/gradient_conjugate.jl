20fbee1455c2d6a15ffde12c7b7ccf4889017d83
Fabien Baron <fbaron@gsu.edu>
2025-10-30 11:28:01 -0400

    Updated gradient descent code

--- src/gradient_conjugate.jl
+++ src/gradient_conjugate.jl
@@ -1,57 +1,64 @@
-using PyPlot
-#f_ack=(x,y)->-20*exp.(-0.2*sqrt.(0.5*(x.^2+y.^2)))-exp.(0.5*(cos.(2*π*x)+cos.(2*π*y))).+exp(1).+20.0
-a=1; b=100;
-f_ros=x->(a .- x[1]).^2 + b*(x[2] - x[1].^2).^2;
-# rr = collect(range(-5,5,length=1001));
-# yy = repeat(rr,1,1001);
-# xx = yy';
-# fmap = f_ros(xx,yy)
-# # Plots
-# clf();imshow(fmap.^.2)
-g_ros = x -> [-2*(a .- x[1]) - 4b*x[1].*(x[2] .- x[1] .^2), 2b*(x[2] .- x[1].^2)]
-
-# h_ros = [ ax+y     x^2+b   ]
-#         [  by+x^2     y^2+x^2   ]
-#
-# h_ros(x[1,:]) = [ 0.1   3.4 ]
-#                 [ 3.5   -1.3]
 using LinearAlgebra
-# inv(h_ros(x[1,:])) =  [ 1 2]
-#                       [ 3 4]
+using PyPlot
+a=1.0; b=100.0; 
+# Rosenbrock
+f =x->(a - x[1])^2 + b*(x[2] - x[1]^2)^2;
+# Analytic gradient (2)
+g = x-> [-2(a-x[1])-4b*(x[2]-x[1]^2)*x[1], 2b*x[2]- 2b*x[1]^2];
 
-function linesearch(x_current, d_current)
-α_try = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0, 10., 100., 1000.]
-fval = zeros(length(α_try))
-for i=1:length(α_try)
-fval[i] = f_ros(x_current - α_try[i] * d_current)
-end
-return α_try[findmin(fval)[2]]
+function linesearch_alpha(x, direction) # note: f and g are predefined
+    ntry = 100 # 100 alphas will be tried
+    α = 10.0.^(range(-5, 5, ntry)) # exponential scale
+    x_new = x .- α'.*direction
+    y_new = [f(x_new[:,i]) for i=1:ntry]
+    return α[findmin(y_new)[2]]
 end
 
-N=100
-f = zeros(N)
-x = zeros(N, 2)
+
+
+N=100; # Number of iterations
+x = zeros(N, 2) # History of the x locations
+fhist = zeros(N) # History of the function values at x
+ghist = zeros(N, 2)
 y = zeros(N, 2)
-g = zeros(N, 2)
 d = zeros(N, 2)
 β = zeros(N)
-x[1,:] .= 10*rand(2).-5
-f[1] = f_ros(x[1,:])
-g[1,:] = g_ros(x[1,:])
-d[1,:] = - g[1,:]
 
-α=1e-4;
+x[1,:] .= [4.0, -2.0]
+fhist[1] =  f(x[1,:])
+ghist[1,:] = g(x[1,:])
+d[1,:] = - ghist[1,:]
+
 for i=2:N
-    g[i,:] = g_ros(x[i,:]);
-    y[i,:] = g[i,:]-g[i-1,:]
-#   β[i] = norm(g[i,:])^2/norm(g[i-1,:])^2; #Fletcher-Reeves
-    β[i] = (g[i,:]'*y[i,:])[1]/norm(g[i-1,:])^2; #Polyak-Ribiere
-    d[i,:] = -g[i,:] + β[i]*d[i-1,:];
-    α = linesearch(x[i-1,:], d[i,:]);
+    ghist[i,:] = g(x[i,:]);
+    y[i,:] = ghist[i,:]-ghist[i-1,:]
+#   β[i] = norm(ghist[i,:])^2/norm(ghist[i-1,:])^2; #Fletcher-Reeves
+    β[i] = (ghist[i,:]'*y[i,:])[1]/norm(ghist[i-1,:])^2; #Polyak-Ribiere
+    d[i,:] = -ghist[i,:] + β[i]*d[i-1,:]
+    α = linesearch_alpha(x[i-1,:], d[i,:])
     x[i,:] = x[i-1,:] + α * d[i,:];
-    f[i] = f_ros(x[i,:]);
+    fhist[i] = f(x[i,:]);
 end
+
+# Visualize the surface
 rr = collect(range(-5,5,length=1000));
-map = [f_ros([i,j]) for i in rr for j in rr]
-imshow(reshape(map.^.2,(1000,1000)))
-scatter((x[:,1].+5)*100, (x[:,2].+5)*100, s=1, color=:red)
+yy = repeat(rr,1,1000); xx=yy';
+map = reshape([f([i,j]) for i in rr for j in rr],(1000,1000))
+#plot_surface(xx, yy, map, cmap="Spectral_r", edgecolor="none")
+figure(figsize=(12,6),"Gradient descent")
+subplot(1,2,1)
+imshow(map.^.2, cmap="Spectral_r");  
+# Initial point
+scatter((x[1,1] +5)*100+1,(x[1,2]+5)*100+1, color=:blue, s=10)
+# History of the points
+scatter((x[2:end-1,1].+5)*100, (x[2:end-1,2].+5)*100, s=1, color=:red)
+# End point
+scatter((x[end,1].+5)*100, (x[end,2].+5)*100, s=10, color=:green)
+subplot(1,2,2)
+scatter(2:N, fhist[2:end])
+xlabel("Iteration number")
+ylabel("Function value f(x)")
+tight_layout();
+
+
+
--- src/gradient_descent.jl
+++ src/gradient_descent.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra
 using PyPlot
-#f_ack=(x,y)->-20*exp.(-0.2*sqrt.(0.5*(x.^2+y.^2)))-exp.(0.5*(cos.(2*π*x)+cos.(2*π*y))).+exp(1).+20.0
 a=1.0; b=100.0; 
 # Rosenbrock
 f =x->(a - x[1])^2 + b*(x[2] - x[1]^2)^2;
@@ -19,8 +18,8 @@ subplot(1,2,1)
 imshow(map.^.2, cmap="Spectral_r");  
 
 N=100; # Number of iterations
-x = zeros(N+1, 2) # History of the x locations
-fhist = zeros(N+1) # History of the function values at x
+x = zeros(N, 2) # History of the x locations
+fhist = zeros(N) # History of the function values at x
 x[1,:] .= [4.0, -2.0]
 fhist[1] =  f(x[1,:])
 # Steepest descent
@@ -28,10 +27,10 @@ fhist[1] =  f(x[1,:])
 α=1e-7
 # low values -> slow 
 # high values -> Inf or NaN
-for n=1:N
+for n=2:N
+    x[n,:] = x[n-1,:] .- α'.*g(x[n-1,:])
+    fhist[n] = f(x[n,:]) 
     println("Iteration $n: x = $(round.(x[n,:], digits=4)), f(x)= $(f(x[n,:]))")
-    x[n+1,:] = x[n,:] .- α'.*g(x[n,:])
-    fhist[n+1] = f(x[n+1,:]) 
 end
 subplot(1,2,1)
 # Initial point
@@ -41,7 +40,7 @@ scatter((x[2:end-1,1].+5)*100, (x[2:end-1,2].+5)*100, s=1, color=:red)
 # End point
 scatter((x[end,1].+5)*100, (x[end,2].+5)*100, s=10, color=:green)
 subplot(1,2,2)
-scatter(1:N+1, fhist)
+scatter(1:N, fhist)
 xlabel("Iteration number")
 ylabel("Function value f(x)")
 tight_layout();
@@ -62,27 +61,27 @@ gca().set_xscale("log")
 
 best_indx = findmin(y_try)[2]
 α[best_indx]
-x_search_min = x_try[:,α_best]
+x_search_min = x_try[:,best_indx]
 
 # Compare these values
 f(x[n,:]) # Original value
 f(x_search_min) # best move we could do
 f(x[n+1,:]) # What we did
 
-function linesearch_alpha(x) # note: f and g are predefined
+function linesearch_alpha(x, direction) # note: f and g are predefined
     ntry = 100 # 100 alphas will be tried
     α = 10.0.^(range(-5, 5, ntry)) # exponential scale
-    x_new = x .- α'.*g(x)
+    x_new = x .- α'.*direction
     y_new = [f(x_new[:,i]) for i=1:ntry]
     return α[findmin(y_new)[2]]
 end
 
 # Now let's redo our gradient descent with line search
-for n=1:N
+for n=2:N
+    α = linesearch_alpha(x[n-1,:], g(x[n-1,:]))
+    x[n,:] = x[n-1,:] - α*g(x[n-1,:])
+    fhist[n] = f(x[n,:])
     println("Iteration $n: x = $(round.(x[n,:], digits=4)), f(x)= $(f(x[n,:]))")
-    α = linesearch_alpha(x[n,:])
-    x[n+1,:] = x[n,:] - α*g(x[n,:])
-    fhist[n+1] = f(x[n,:])
 end
 
 # Visualize the surface
@@ -90,7 +89,7 @@ rr = collect(range(-5,5,length=1000));
 yy = repeat(rr,1,1000); xx=yy';
 map = reshape([f([i,j]) for i in rr for j in rr],(1000,1000))
 #plot_surface(xx, yy, map, cmap="Spectral_r", edgecolor="none")
-figure(figsize=(12,6),"Gradient descent")
+figure(figsize=(12,6),"Gradient descent"); clf()
 subplot(1,2,1)
 imshow(map.^.2, cmap="Spectral_r");  
 # Initial point
@@ -100,7 +99,7 @@ scatter((x[2:end-1,1].+5)*100, (x[2:end-1,2].+5)*100, s=1, color=:red)
 # End point
 scatter((x[end,1].+5)*100, (x[end,2].+5)*100, s=10, color=:green)
 subplot(1,2,2)
-scatter(2:N, fhist[2:end])
+scatter(1:N, fhist)
 xlabel("Iteration number")
 ylabel("Function value f(x)")
 tight_layout();
